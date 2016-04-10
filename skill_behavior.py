86fb25e897562dd51b155b82b31887fc32cc3e67
cmclen <chris.mclennon@civitaslearning.com>
2016-04-10 10:22:50 -0500

    Version 8

--- skill_behavior.py
+++ skill_behavior.py
@@ -17,7 +17,7 @@ def get_welcome_response():
 
     card_title = "Welcome to AlexaRPG"
     # debug output
-    speech_output = 'Version 7. ' + \
+    speech_output = 'Version 8. ' + \
         session_attributes['scene'][session_attributes['currentScene']+'+load']['description']
     #
     # sexy output
